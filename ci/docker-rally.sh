3a2272f1148dd8cac3d387c061c3143dd639cb17
Kyle Mestery <mestery@mestery.com>
2016-07-15 18:38:23 +0000

    Add error handling
    Add some initial error handling into the CI scripts
    
    Closes Issue #69
    
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- ci/docker-rally.sh
+++ ci/docker-rally.sh
@@ -4,6 +4,9 @@
 XTRACE=$(set +o | grep xtrace)
 set -o xtrace
 
+# Track failures
+FAILED=0
+
 OVS_REPO=${1:-https://github.com/openvswitch/ovs.git}
 OVS_BRANCH=${2:-master}
 
@@ -12,13 +15,15 @@ echo "OVS_REPO=${OVS_REPO} OVS_BRANCH=${OVS_BRANCH}"
 # A combined script to run all the things
 
 # Prepare the environment
-./prepare.sh
+./prepare.sh || FAILED=$(( $FAILED + 1 ))
 
 # Run the testsuite
-./scale-run.sh $OVS_REPO $OVS_BRANCH
+./scale-run.sh $OVS_REPO $OVS_BRANCH || FAILED=$(( $FAILED + 1 ))
 
 # Clean things up
-./scale-cleanup.sh
+./scale-cleanup.sh || FAILED=$(( $FAILED + 1 ))
+
+return $FAILED
 
 # Restore xtrace
 $XTRACE
