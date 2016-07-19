91cfbe5a119112864a8de626d90e77404c763f4e
Ryan Moats <rmoats@us.ibm.com>
2016-07-19 15:19:52 -0500

    Fix a typo in scale-run.sh
    Configure flags use configflags, not configflag
    
    Signed-off-by: Ryan Moats <rmoats@us.ibm.com>

--- ci/scale-run.sh
+++ ci/scale-run.sh
@@ -9,7 +9,7 @@ source ovn-scale.conf
 
 OVS_REPO=$1
 OVS_BRANCH=$2
-CONFIG_FLAG=$3
+CONFIG_FLAGS=$3
 
 function check_container_failure {
     sleep 5
@@ -28,7 +28,7 @@ function check_container_failure {
 # Build the docker containers
 pushd $OVN_SCALE_TOP
 cd ansible/docker
-make ovsrepo=$OVS_REPO ovsbranch=$OVS_BRANCH configflag=$CONFIG_FLAG
+make ovsrepo=$OVS_REPO ovsbranch=$OVS_BRANCH configflags=$CONFIG_FLAGS
 popd
 $OVNSUDO docker images
 
@@ -38,7 +38,7 @@ $OVNSUDO docker images
 #                that assumption changes.
 pushd $OVN_SCALE_TOP
 $OVNSUDO /usr/local/bin/ansible-playbook -i $OVN_DOCKER_HOSTS ansible/site.yml -e @$OVN_DOCKER_VARS \
-     --extra-vars "ovs_repo=$OVS_REPO" --extra-vars "ovs_branch=$OVS_BRANCH" --extra-vars "config_flag=$CONFIG_FLAG" -e action=deploy
+     --extra-vars "ovs_repo=$OVS_REPO" --extra-vars "ovs_branch=$OVS_BRANCH" --extra-vars "configflags=$CONFIG_FLAGS" -e action=deploy
 if [ "$?" != "0" ] ; then
     echo "Deploying failed, exiting"
     exit 1
