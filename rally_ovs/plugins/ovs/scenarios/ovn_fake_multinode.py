514b45d60328fc834ae3cdacceb79d2a87e1709a
Dumitru Ceara <dceara@redhat.com>
2020-03-06 16:54:24 +0100

    Add Openshift-like 'switch per node' bring-up scenario.
    Add new OVN scenario OvnNorthboundFakeMultinode which implements a test
    case that brings up and configures ovn-fake-multinode nodes
    (logical_switch, connect to logical_router, configure mgmt port, ping
    from mgmt port to default gateway).
    
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- rally_ovs/plugins/ovs/scenarios/ovn_fake_multinode.py
+++ rally_ovs/plugins/ovs/scenarios/ovn_fake_multinode.py
@@ -16,6 +16,7 @@ from rally_ovs.plugins.ovs.scenarios import ovn
 from rally.task import atomic
 from rally.task import scenario
 import time
+import netaddr
 
 """Scenario to dynamically add/delete ovn nodes running in containers.
 
@@ -41,52 +42,60 @@ class OvnFakeMultinode(ovn.OvnScenario):
         ssh.enable_batch_mode(False)
         return ssh
 
-    @atomic.action_timer("OvnFakeMultinode.add_central_node")
     def _add_central(self, ssh_conn, node_net, node_net_len, node_ip,
-                     ovn_fake_path):
-        cmd = "cd {} && CHASSIS_COUNT=0 GW_COUNT=0 IP_HOST={} IP_CIDR={} IP_START={} ./ovn_cluster.sh start || true".format(
-            ovn_fake_path, node_net, node_net_len, node_ip
+                     ovn_fake_path, monitor_all=False):
+        if monitor_all:
+            monitor_cmd = "OVN_MONITOR_ALL=yes"
+        else:
+            monitor_cmd = "OVN_MONITOR_ALL=no"
+
+        cmd = "cd {} && CHASSIS_COUNT=0 GW_COUNT=0 IP_HOST={} IP_CIDR={} IP_START={} {} CREATE_FAKE_VMS=no ./ovn_cluster.sh start".format(
+            ovn_fake_path, node_net, node_net_len, node_ip, monitor_cmd
         )
         ssh_conn.run(cmd)
 
-    @atomic.action_timer("OvnFakeMultinode.add_chassis_node")
+        time.sleep(5)
+
     def _add_chassis(self, ssh_conn, node_net, node_net_len, node_ip, node_name,
-                     ovn_fake_path):
+                     ovn_fake_path, monitor_all=False):
         invalid_remote = "tcp:0.0.0.1:6642"
-        cmd = "cd {} && IP_HOST={} IP_CIDR={} IP_START={} ./ovn_cluster.sh add-chassis {} {} || true".format(
-            ovn_fake_path, node_net, node_net_len, node_ip, node_name, invalid_remote
+        if monitor_all:
+            monitor_cmd = "OVN_MONITOR_ALL=yes"
+        else:
+            monitor_cmd = "OVN_MONITOR_ALL=no"
+
+        cmd = "cd {} && IP_HOST={} IP_CIDR={} IP_START={} {} ./ovn_cluster.sh add-chassis {} {}".format(
+            ovn_fake_path, node_net, node_net_len, node_ip, monitor_cmd,
+            node_name, invalid_remote
         )
         ssh_conn.run(cmd)
 
-    @atomic.action_timer("OvnFakeMultinode.connect_chassis_node")
     def _connect_chassis(self, ssh_conn, node_name, central_ip, ovn_fake_path):
-        cmd = "cd {} && ./ovn_cluster.sh set-chassis-ovn-remote {} tcp:{}:6642 || true".format(
+        cmd = "cd {} && ./ovn_cluster.sh set-chassis-ovn-remote {} tcp:{}:6642".format(
             ovn_fake_path, node_name, central_ip
         )
         ssh_conn.run(cmd)
 
-    @atomic.action_timer("ovnFakeMultinode.wait_chassis_node")
     def _wait_chassis(self, sbctl_conn, chassis_name, max_timeout_s):
         for i in range(0, max_timeout_s * 10):
             if sbctl_conn.chassis_bound(chassis_name):
                 break
             time.sleep(0.1)
 
-    @atomic.action_timer("OvnFakeMultinode.del_chassis_node")
     def _del_chassis(self, ssh_conn, node_name, ovn_fake_path):
-        cmd = "cd {} && ./ovn_cluster.sh stop-chassis {} || true".format(
+        cmd = "cd {} && OVN_BR_CLEANUP=no ./ovn_cluster.sh stop-chassis {}".format(
             ovn_fake_path, node_name
         )
         ssh_conn.run(cmd)
 
-    @atomic.action_timer("OvnFakeMultinode.del_central_node")
     def _del_central(self, ssh_conn, ovn_fake_path):
-        cmd = "cd {} && CHASSIS_COUNT=0 GW_COUNT=0 ./ovn_cluster.sh stop || true".format(
+        cmd = "cd {} && CHASSIS_COUNT=0 GW_COUNT=0 OVN_BR_CLEANUP=no ./ovn_cluster.sh stop".format(
             ovn_fake_path
         )
         ssh_conn.run(cmd)
 
     @scenario.configure(context={})
+    @atomic.action_timer("OvnFakeMultinode.add_central_node")
     def add_central_node(self, fake_multinode_args = {}):
         ssh = self.controller_client("ovs-ssh")
         ssh.set_sandbox("controller-sandbox", self.install_method)
@@ -96,10 +105,13 @@ class OvnFakeMultinode(ovn.OvnScenario):
         node_net_len = fake_multinode_args.get("node_net_len")
         node_ip = fake_multinode_args.get("node_ip")
         ovn_fake_path = fake_multinode_args.get("cluster_cmd_path")
+        monitor_all = fake_multinode_args.get("ovn_monitor_all")
 
-        self._add_central(ssh, node_net, node_net_len, node_ip, ovn_fake_path)
+        self._add_central(ssh, node_net, node_net_len, node_ip, ovn_fake_path,
+                          monitor_all)
 
     @scenario.configure(context={})
+    @atomic.action_timer("OvnFakeMultinode.add_chassis_node")
     def add_chassis_node(self, fake_multinode_args = {}):
         farm = fake_multinode_args.get("farm")
         sb = self._get_sandbox(farm)
@@ -110,10 +122,13 @@ class OvnFakeMultinode(ovn.OvnScenario):
         node_ip = fake_multinode_args.get("node_ip")
         node_name = sb["host_container"]
         ovn_fake_path = fake_multinode_args.get("cluster_cmd_path")
+        monitor_all = fake_multinode_args.get("ovn_monitor_all")
+
         self._add_chassis(ssh, node_net, node_net_len, node_ip, node_name,
-                          ovn_fake_path)
+                          ovn_fake_path, monitor_all)
 
     @scenario.configure(context={})
+    @atomic.action_timer("OvnFakeMultinode.connect_chassis_node")
     def connect_chassis_node(self, fake_multinode_args = {}):
         farm = fake_multinode_args.get("farm")
         sb = self._get_sandbox(farm)
@@ -125,9 +140,10 @@ class OvnFakeMultinode(ovn.OvnScenario):
         self._connect_chassis(ssh, node_name, central_ip, ovn_fake_path)
 
     @scenario.configure(context={})
+    @atomic.action_timer("ovnFakeMultinode.wait_chassis_node")
     def wait_chassis_node(self, fake_multinode_args = {}):
         farm = fake_multinode_args.get("farm")
-        max_timeout_s = fake_multinode_args.get("max_timeout_s")
+        max_timeout_s = fake_multinode_args.get("max_timeout_s", 60)
         sb = self._get_sandbox(farm)
         node_name = sb["host_container"]
 
@@ -138,6 +154,7 @@ class OvnFakeMultinode(ovn.OvnScenario):
         self._wait_chassis(ovn_sbctl, node_name, max_timeout_s)
 
     @scenario.configure(context={})
+    @atomic.action_timer("OvnFakeMultinode.del_chassis_node")
     def del_chassis_node(self, fake_multinode_args = {}):
         farm = fake_multinode_args.get("farm")
         sb = self._get_sandbox(farm)
@@ -148,6 +165,7 @@ class OvnFakeMultinode(ovn.OvnScenario):
         self._del_chassis(ssh, node_name, ovn_fake_path)
 
     @scenario.configure(context={})
+    @atomic.action_timer("OvnFakeMultinode.del_central_node")
     def del_central_node(self, fake_multinode_args = {}):
         ssh = self.controller_client("ovs-ssh")
         ssh.set_sandbox("controller-sandbox", self.install_method)
@@ -155,3 +173,115 @@ class OvnFakeMultinode(ovn.OvnScenario):
 
         ovn_fake_path = fake_multinode_args.get("cluster_cmd_path")
         self._del_central(ssh, ovn_fake_path)
+
+    @scenario.configure(context={})
+    @atomic.action_timer("OvnFakeMultinode.add_chassis_nodes")
+    def add_chassis_nodes(self, fake_multinode_args = {}):
+        iteration = self.context["iteration"]
+        batch_size = fake_multinode_args.get("batch_size", 1)
+        node_prefix = fake_multinode_args.get("node_prefix", "")
+
+        node_net = fake_multinode_args.get("node_net")
+        node_net_len = fake_multinode_args.get("node_net_len")
+        node_cidr = netaddr.IPNetwork("{}/{}".format(node_net, node_net_len))
+
+        monitor_all = fake_multinode_args.get("ovn_monitor_all")
+        ovn_fake_path = fake_multinode_args.get("cluster_cmd_path")
+
+        for i in range(batch_size):
+            index = iteration * batch_size + i
+            node_ip = str(node_cidr.ip + index + 1)
+
+            farm = "{}{}".format(node_prefix, index % len(self._sandboxes))
+            sb = self._get_sandbox(farm)
+            ssh = self._get_sandbox_conn(sb["name"], sb)
+            node_name = sb["host_container"]
+
+            self._add_chassis(ssh, node_net, node_net_len, node_ip, node_name,
+                              ovn_fake_path, monitor_all)
+
+    @scenario.configure(context={})
+    @atomic.action_timer("OvnFakeMultinode.connect_chassis_nodes")
+    def connect_chassis_nodes(self, fake_multinode_args = {}):
+        iteration = self.context["iteration"]
+        batch_size = fake_multinode_args.get("batch_size", 1)
+        node_prefix = fake_multinode_args.get("node_prefix", "")
+
+        central_ip = fake_multinode_args.get("central_ip")
+        ovn_fake_path = fake_multinode_args.get("cluster_cmd_path")
+
+        for i in range(batch_size):
+            index = iteration * batch_size + i
+
+            farm = "{}{}".format(node_prefix, index % len(self._sandboxes))
+            sb = self._get_sandbox(farm)
+            ssh = self._get_sandbox_conn(sb["name"], sb)
+            node_name = sb["host_container"]
+
+            self._connect_chassis(ssh, node_name, central_ip, ovn_fake_path)
+
+    @scenario.configure(context={})
+    @atomic.action_timer("ovnFakeMultinode.wait_chassis_nodes")
+    def wait_chassis_nodes(self, fake_multinode_args = {}):
+        iteration = self.context["iteration"]
+        batch_size = fake_multinode_args.get("batch_size", 1)
+        node_prefix = fake_multinode_args.get("node_prefix", "")
+        max_timeout_s = fake_multinode_args.get("max_timeout_s")
+
+        ovn_sbctl = self.controller_client("ovn-sbctl")
+        ovn_sbctl.set_sandbox("controller-sandbox", self.install_method,
+                              self.context['controller']['host_container'])
+        ovn_sbctl.enable_batch_mode(False)
+        for i in range(batch_size):
+            index = iteration * batch_size + i
+
+            farm = "{}{}".format(node_prefix, index % len(self._sandboxes))
+            sb = self._get_sandbox(farm)
+            ssh = self._get_sandbox_conn(sb["name"], sb)
+            node_name = sb["host_container"]
+
+            self._wait_chassis(ovn_sbctl, node_name, max_timeout_s)
+
+    @scenario.configure(context={})
+    @atomic.action_timer("OvnFakeMultinode.del_chassis_nodes")
+    def del_chassis_nodes(self, fake_multinode_args = {}):
+        iteration = self.context["iteration"]
+        batch_size = fake_multinode_args.get("batch_size", 1)
+        node_prefix = fake_multinode_args.get("node_prefix", "")
+
+        for i in range(batch_size):
+            index = iteration * batch_size + i
+
+            farm = "{}{}".format(node_prefix, index % len(self._sandboxes))
+            sb = self._get_sandbox(farm)
+            ssh = self._get_sandbox_conn(sb["name"], sb)
+
+            ovn_fake_path = fake_multinode_args.get("cluster_cmd_path")
+            node_name = sb["host_container"]
+
+            self._del_chassis(ssh, node_name, ovn_fake_path)
+
+
+class OvnNorthboundFakeMultinode(OvnFakeMultinode):
+
+    def __init__(self, context):
+        super(OvnNorthboundFakeMultinode, self).__init__(context)
+
+    @scenario.configure(context={})
+    def setup_switch_per_node(self, fake_multinode_args = {},
+                              lswitch_create_args = {},
+                              networks_per_router = {},
+                              lport_create_args = {},
+                              port_bind_args = {},
+                              create_mgmt_port = True):
+        self.add_chassis_nodes(fake_multinode_args)
+        self.connect_chassis_nodes(fake_multinode_args)
+        self.wait_chassis_nodes(fake_multinode_args)
+
+        lswitch_create_args['amount'] = fake_multinode_args.get('batch_size', 1)
+        lswitch_create_args['batch'] = 1
+        self._create_routed_network(lswitch_create_args=lswitch_create_args,
+                                    networks_per_router=networks_per_router,
+                                    lport_create_args=lport_create_args,
+                                    port_bind_args=port_bind_args,
+                                    create_mgmt_port=create_mgmt_port)
